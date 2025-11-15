29994dce7f055908964d8d61a503eec783a6ae53
Ankit Jha <mrankitjha007@gmail.com>
2025-11-15 19:22:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+// import MedicalAnalyzer from './components/MedicalAnalyzer'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>🧬 DocTalk</h1>
+        <p>AI-Powered Medical Report Translation</p>
+      </header>
+      
+      {/* <main>
+       // <MedicalAnalyzer />
+      </main> */}
+    </div>
+  )
+}
+
+export default App
